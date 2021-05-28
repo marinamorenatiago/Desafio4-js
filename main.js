0116552d41169331ca1abe16e3ff148670d3c58c
marinamorenatiago <morenatiago@gmail.com>
2021-05-27 21:21:10 -0300

    subindo desafio 4 js

---
+++ main.js
@@ -0,0 +1,35 @@
+function mensagem(){
+    console.log("Olá!")
+}
+
+mensagem()
+
+function desafioquatro(nome){
+    console.log(`Meu nome é ${nome}`)
+}
+desafioquatro("Marina")
+
+function conhecendo(nome, idade, estilomusical){
+    console.log(`Meu nome é ${nome}, eu tenho ${idade} anos de idade e meu estilo musical preferido é ${estilomusical}`)
+}
+conhecendo("Marina", 21, "MPB")
+
+function conhecendomais(filme, musica){
+    console.log(`Meu filme favorito é ${filme} e minha música favorita é ${musica}`)
+}
+conhecendomais("Um amor para recordar", "Gerânio da Marisa Monte")
+
+function tabuadadetres(x){
+    return x * 3
+}
+
+console.log(tabuadadetres(1))
+console.log(tabuadadetres(2))
+console.log(tabuadadetres(3))
+console.log(tabuadadetres(4))
+console.log(tabuadadetres(5))
+console.log(tabuadadetres(6))
+console.log(tabuadadetres(7))
+console.log(tabuadadetres(8))
+console.log(tabuadadetres(9))
+console.log(tabuadadetres(10))
